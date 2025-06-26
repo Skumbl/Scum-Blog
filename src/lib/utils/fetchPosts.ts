1f48fb0e88234dfb33e83f299a668954c0049ef6
Scumble <janarvik01@pm.me>
2025-06-25 23:35:08 -0400

    latestPost added slug to post data

---
+++ src/lib/utils/fetchPosts.ts
@@ -0,0 +1,42 @@
+import type { MarkdownModule, PostData } from '$lib/types/post';
+
+// private function to get all posts
+async function fetchAllPosts(): Promise<PostData[]> {
+	const modules = import.meta.glob<MarkdownModule>(
+		'$lib/assets/posts/*.md',
+		{ eager: true }
+	);
+
+	const posts: PostData[] = [];
+
+	for (const [path, module] of Object.entries(modules)) {
+		const fileName = path.split('/').pop()?.replace('.md', '') ?? '';
+		posts.push({
+			slug: fileName,
+			...module.metadata,
+			Content: module.default
+		});
+	}
+	return posts;
+}
+
+export async function getAllPosts(): Promise<PostData[]> {
+	const posts: PostData[] = await fetchAllPosts();
+
+	return posts.sort(
+		(a: PostData, b: PostData): number =>
+			new Date(b.date).getTime() - new Date(a.date).getTime()
+	);
+}
+
+export async function getLatestPost(): Promise<PostData | null> {
+	const posts: PostData[] = await fetchAllPosts();
+
+	if (posts.length === 0) {
+		return null;
+	}
+
+	return posts.sort(
+		(a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+	)[0];
+}
--- src/routes/blog/[slug]/+page.ts
+++ src/routes/blog/[slug]/+page.ts
@@ -3,11 +3,13 @@ import { error } from '@sveltejs/kit';
 
 export const load = async ({ params }) => {
 	try {
-		const postModule: MarkdownModule = await import(`$lib/assets/posts/${params.slug}.md`);
+		const slug = params.slug;
+		const postModule: MarkdownModule = await import(`$lib/assets/posts/${slug}.md`);
 		const { metadata, default: Content } = postModule;
 
 		//PostData
 		return {
+			slug,
 			...metadata,
 			Content,
 		};
